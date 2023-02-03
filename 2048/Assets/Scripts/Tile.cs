1725e5c39addbb37a54a614b9fb693fde7f9c694
Shreyas Borakhadikar <shreyasb1511@gmail.com>
2023-02-03 20:24:14 +0530

    Added tile scripts

---
+++ 2048/Assets/Scripts/Tile.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public TileState state { get; private set; }
+    public TileCell cell { get; private set; }
+    public int number { get; private set; }
+}
---
+++ 2048/Assets/Scripts/TileGrid.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGrid : MonoBehaviour
+{
+    public TileRow[] rows { get; private set; }
+    public TileCell[] cells { get; private set; }
+
+    public int size => cells.Length;
+    public int height => rows.Length;
+    public int width => size / height;
+
+    private void Awake()
+    {
+        rows = GetComponentsInChildren<TileRow>();
+        cells = GetComponentsInChildren<TileCell>();
+    }
+
+    private void Start()
+    {
+        for(int y = 0; y < rows.Length; y++)
+        {
+            for(int x = 0; x < rows[y].cells.Length; x++)
+            {
+                rows[y].cells[x].coordinates = new Vector2Int(x, y);
+            }
+        }
+    }
+}
