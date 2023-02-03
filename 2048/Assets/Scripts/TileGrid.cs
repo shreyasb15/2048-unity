5ad34ea002c2b3e1b785a0199ccdb4975ef55125
Shreyas Borakhadikar <shreyasb1511@gmail.com>
2023-02-03 22:01:48 +0530

    Added scripts to tiles

--- 2048/Assets/Scripts/TileGrid.cs
+++ 2048/Assets/Scripts/TileGrid.cs
@@ -27,4 +27,27 @@ public class TileGrid : MonoBehaviour
             }
         }
     }
+
+    public TileCell GetRandomEmptyCell()
+    {
+        int index = Random.Range(0, cells.Length);
+        int startingIndex = index;
+
+        while(cells[index].occupied)
+        {
+            index++;
+
+            if(index >= cells.Length)
+            {
+                index = 0;
+            }
+
+            if(index == startingIndex)
+            {
+                return null;
+            }
+        }
+
+        return cells[index];
+    }
 }
